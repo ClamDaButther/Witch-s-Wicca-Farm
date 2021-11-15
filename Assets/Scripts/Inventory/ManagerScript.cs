d7d60ad487a06899a742065b0e1e7220cdec27d5
ClamDaButther <ClamDaButther@gmail.com>
2021-11-15 16:37:21 +0100

    BeginManagerScript
    BeginManagerScript

---
+++ Assets/Scripts/Inventory/ManagerScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+    public static int HerbCount;
+    public static int PotatoCount;
+    public static int TuberCount;
+    public static int GlowingMushroomCount;
+
+    public static bool HomeGate;
+
+    public static bool MainQuest;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
